9ef64b851089a7c665e517a7e714cf987a2488b0
CrizZound <cristian.alvarez.urra@gmail.com>
2025-06-22 02:30:13 -0400

    Spinners agregados

--- src/app/pages/registrohogar/registrohogar.component.ts
+++ src/app/pages/registrohogar/registrohogar.component.ts
@@ -7,22 +7,27 @@ import { MatCardModule } from '@angular/material/card';
 import { FirebaseService } from '../../../firebase.service';
 import { getAuth } from 'firebase/auth';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-registrohogar',
   standalone: true,
-  imports: [HeaderComponent,FooterComponent,ReactiveFormsModule,MatCardModule,FormsModule,MatProgressSpinnerModule],
+  imports: [HeaderComponent,FooterComponent,ReactiveFormsModule,MatCardModule,FormsModule,MatProgressSpinnerModule,
+            NgIf,NgFor
+  ],
   templateUrl: './registrohogar.component.html',
   styleUrl: './registrohogar.component.css'
 })
 export class RegistrohogarComponent {
   nombreHogar: string = '';
+  isLoading: boolean = false;
 
   constructor(private firebaseservice: FirebaseService, private router: Router){
     
   }
 
   async registrarHogar() {
+    this.isLoading = true;
     try {
       const auth = getAuth();
       const user = auth.currentUser;
@@ -38,6 +43,7 @@ export class RegistrohogarComponent {
     } catch (error: any) {
       console.error('Error de registro:', error.message);
     }
+    this.isLoading = false;
   }
 
 }
